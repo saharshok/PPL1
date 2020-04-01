53a2263b95bf4133f01c732e68d8036e5561cefc
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2020-04-01 19:55:37 +0300

    fixed a badly commited file

--- test/part2.test.ts
+++ test/part2.test.ts
@@ -2,13 +2,7 @@ import { expect } from "chai";
 
 import { partition, mapMat } from "../src/part2/part2"
 
-<<<<<<< HEAD
-import { mapMat } from "../src/part2/part2"
-
-function biggerthen10(element:number) { 
-=======
 function biggerthen10(element:number) {
->>>>>>> 58e92dc855a075fd8762628eba68784b4f672576
     return (element >= 10);
 }
 
@@ -24,32 +18,16 @@ describe("Assignment 1 Part 2 Q:1 (partition)", () => {
     it("check 2",()=> {
         expect(partition(x => x % 2 === 0, numbers)).to.eql([[2, 4, 6, 8], [1, 3, 5, 7, 9]]);
     });
-<<<<<<< HEAD
-})
-
-
-describe("Assignment 1 Part 2 Q:2", () => {
-=======
 });
 
 describe("Assignment 1 Part 2 Q:2 (mapMat)", () => {
->>>>>>> 58e92dc855a075fd8762628eba68784b4f672576
     let result =  [[ 1, 4, 9 ], [ 16, 25, 36 ], [ 49, 64, 81 ]];
     let start =  [
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9]
-<<<<<<< HEAD
-        ]
-        ;
-    it("chack modle 1 ",()=> {
-        expect(mapMat(x => x * x, start)).to.eql(result);
-    });
-})
-=======
     ];
     it("check 1 ", () => {
         expect(mapMat(x => x * x, start)).to.eql(result);
     });
 });
->>>>>>> 58e92dc855a075fd8762628eba68784b4f672576
