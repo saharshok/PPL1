daac9fad7f31952185fa1ccd1b130f940db2bf32
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2020-04-05 16:40:46 +0300

    removed redundant stuff, change None<T> to be non-generic and changed the signature of makeSome and makeNone to return Optional<T>

--- src/part3/optional.ts
+++ src/part3/optional.ts
@@ -1,25 +1,22 @@
 /* Question 1 */
-
-import { values } from "ramda";
-
 interface Some<T> {
     tag: "Some";
     value: T;
 }
 
-interface None<T> {
+interface None {
     tag: "None";
 }
 
-export type Optional<T> = Some<T> | None<T>;
+export type Optional<T> = Some<T> | None;
 
-export const makeSome: <T>(value: T) => Some<T> = <T>(value: T): Some<T> => ({tag: "Some",value: value});
+export const makeSome: <T>(value: T) => Optional<T> = <T>(value: T): Optional<T> => ({tag: "Some",value: value});
 
-export const makeNone: <T>() => None<T> =  <T>(): None<T> => ({tag: "None"});
+export const makeNone: <T>() => Optional<T> =  <T>(): Optional<T> => ({tag: "None"});
 
 export const isSome: <T>(x: Optional<T>) => x is Some<T> = <T>(x: Optional<T>): x is Some<T> => x.tag === "Some";
 
-export const isNone: <T>(x: Optional<T>) => x is None<T> =  <T>(x: Optional<T>): x is None<T> => x.tag === "None";
+export const isNone: <T>(x: Optional<T>) => x is None = <T>(x: Optional<T>): x is None => x.tag === "None";
 
 /* Question 2 */
 export const bind = <T, U>(optional: Optional<T>, f: (x: T) => Optional<U>): Optional<U> => {
